40c63019cbd681c1c2d5349dcde52d8f93799d13
RivaPau <70348732+RivaPau@users.noreply.github.com>
2020-08-27 18:19:47 -0300

    Initial Upload

---
+++ js/webjs.js
@@ -0,0 +1,37 @@
+console.log("JS initializing");
+
+/*$(document).ready(function(){
+    $('.speech-bubble').hide();
+
+    $('.imginsta').on("mouseover", function(){
+        $('.speech-bubble').css("background-image", "url(../assets/img/socialmedia/globe-ig.png)");      
+        $('.speech-bubble').css("background-image", "url(../assets/img/socialmedia/globe-ig.png)");  
+        $('.speech-bubble').show();
+    }).on("mouseout", function(){$('.speech-bubble').hide();})
+
+    $('.imgfb').on("mouseover", function(){
+        $('.speech-bubble').css("background-image", "url(../assets/img/socialmedia/globe-fb.png)");      
+        $('.speech-bubble').show();
+    }).on("mouseout", function(){$('.speech-bubble').hide();})
+
+    $('.imgtt').on("mouseover", function(){
+        $('.speech-bubble').css("background-image", "url(../assets/img/socialmedia/globe-tt.png)");      
+        $('.speech-bubble').show();
+    }).on("mouseout", function(){$('.speech-bubble').hide();})
+    
+    $('.imgdeviant').on("mouseover", function(){
+        $('.speech-bubble').css("background-image", "url(../assets/img/socialmedia/globe-da.png)");      
+        $('.speech-bubble').show();
+    }).on("mouseout", function(){$('.speech-bubble').hide();})
+
+    $('.imgpixilart').on("mouseover", function(){
+        $('.speech-bubble').css("background-image", "url(../assets/img/socialmedia/globe-px.png)");      
+        $('.speech-bubble').show();
+    }).on("mouseout", function(){$('.speech-bubble').hide();})
+
+    $('.imgartstation').on("mouseover", function(){
+        $('.speech-bubble').css("background-image", "url(../assets/img/socialmedia/globe-as.png)");      
+        $('.speech-bubble').show();
+    }).on("mouseout", function(){$('.speech-bubble').hide();})
+})
+*/
